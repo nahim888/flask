5ee073d0e034986563794517a5e2ef7e6768513a
nahim888 <ahmed.nahim@iisgalvanimi.edu.it>
2022-02-22 12:31:14 +0000

    primo progetto

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    return render_template("index.html")
+
+@app.route('/it', methods=['GET'])
+def ciao_mondo():
+    return ('<h1>Ciao, Mondo!</h1>')
+
+@app.route('/fr', methods=['GET'])
+def bonjour_monde():
+    return ('<h1>Bonjour, Monde!</h1>')
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
